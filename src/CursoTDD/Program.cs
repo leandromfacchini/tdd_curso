319cc2f35c2f0ac5d53e7de88b69d1bc7c6fd88d
Leandro Facchini <leandromfacchini@gmail.com>
2019-05-14 22:17:50 -0300

    Início do curso

---
+++ src/CursoTDD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CursoTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("Wonderful life Katie Melua"));
+            Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("life wonderful Katie Melua"));
+            Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("Katie Meluw Wonderful life "));
+
+            Console.ReadKey();
+        }
+    }
+}
